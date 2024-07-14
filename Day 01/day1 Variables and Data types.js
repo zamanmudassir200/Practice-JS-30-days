03bc17ffd179442983fa0b0f0a866120346fd98f
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-14 14:04:14 +0500

    final commit

---
+++ Day 01/day1 Variables and Data types.js
@@ -0,0 +1,51 @@
+//? Activity 1: Variable declaration
+//? Task 1:
+var num = 10;
+console.log(num);
+
+//? Task 2:
+let myName = "Mudassir";
+console.log(myName);
+// ? Activity 2 : Constant Declaration:
+// ? Task 3:
+const isAbsent = true;
+console.log(isAbsent);
+
+// ? Activity 3 : Data Types:
+// ? Task 4:
+let number = 100;
+let fullName = "Mudassir Zaman";
+let isCompleted = false;
+let student = {
+  name: "M.MUDASSIR ZAMAN",
+  age: 23,
+  address: "XYZ ",
+};
+let marks = [70, 56, 77, 90, 45, 87];
+console.log(typeof number);
+console.log(typeof fullName);
+console.log(typeof isCompleted);
+console.log(typeof student);
+console.log(typeof marks);
+
+// ? Activity 4: Reassigning Variables:
+// ? Task 5:
+let age = 20;
+console.log(age);
+age = 23;
+console.log(age);
+// ? Activity 5: Understanding const:
+// ? Task 6:
+const COLOR = "red";
+// COLOR = "green";
+console.log(COLOR);
+/*TypeError: Assignment to constant variable.
+    at Object.<anonymous> (d:\Practice Js\Day 01\day1.js:42:7)
+    at Module._compile (node:internal/modules/cjs/loader:1358:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1416:10)
+    at Module.load (node:internal/modules/cjs/loader:1208:32)
+    at Module._load (node:internal/modules/cjs/loader:1024:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
+    at node:internal/main/run_main_module:28:49
+
+Node.js v20.13.1 */
