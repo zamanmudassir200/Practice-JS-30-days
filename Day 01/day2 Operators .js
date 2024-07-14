f3396e498ac3d9c5e4589963ab2e11651dc5062b
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-14 14:04:35 +0500

    initial commit

---
+++ Day 01/day2 Operators .js
@@ -0,0 +1,31 @@
+// Operators
+// ? Arithmetic Operators
+// ? Task 1: Addition program
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log(sum); // 15
+
+//? Task 2: Subtraction program
+let a = 5;
+let b = 10;
+let subtract = a - b;
+console.log(subtract); // -5
+
+//? Task 3: Multiplication program
+let x = 2;
+let y = 5;
+let mul = x * y;
+console.log(mul); //10
+
+//? Task 2: Division program
+let n1 = 10;
+let n2 = 2;
+let divide = n1 / n2;
+console.log(divide); // 5
+
+//? Task 2: Remainder program
+let x1 = 35;
+let x2 = 10;
+let remainder = x1 % x2;
+console.log(remainder); // 5
