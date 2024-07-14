6d43c8236f87991a60ded3e19cead0484b0c5230
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-14 14:18:50 +0500

    final commit

--- Day 01/day2 Operators .js
+++ Day 01/day2 Operators .js
@@ -1,31 +1,97 @@
-// Operators
-// ? Arithmetic Operators
-// ? Task 1: Addition program
-let num1 = 5;
-let num2 = 10;
-let sum = num1 + num2;
-console.log(sum); // 15
+// // Operators
+// // ? Activity 1: Arithmetic Operators
+// // ? Task 1: Addition program
+// let num1 = 5;
+// let num2 = 10;
+// let sum = num1 + num2;
+// console.log(sum); // 15
 
-//? Task 2: Subtraction program
-let a = 5;
-let b = 10;
-let subtract = a - b;
-console.log(subtract); // -5
+// //? Task 2: Subtraction program
+// let a = 5;
+// let b = 10;
+// let subtract = a - b;
+// console.log(subtract); // -5
 
-//? Task 3: Multiplication program
-let x = 2;
-let y = 5;
-let mul = x * y;
-console.log(mul); //10
+// //? Task 3: Multiplication program
+// let x = 2;
+// let y = 5;
+// let mul = x * y;
+// console.log(mul); //10
 
-//? Task 2: Division program
-let n1 = 10;
-let n2 = 2;
-let divide = n1 / n2;
-console.log(divide); // 5
+// //? Task 2: Division program
+// let n1 = 10;
+// let n2 = 2;
+// let divide = n1 / n2;
+// console.log(divide); // 5
 
-//? Task 2: Remainder program
-let x1 = 35;
-let x2 = 10;
-let remainder = x1 % x2;
-console.log(remainder); // 5
+// //? Task 2: Remainder program
+// let x1 = 35;
+// let x2 = 10;
+// let remainder = x1 % x2;
+// console.log(remainder); // 5
+
+// ? Activity 2: Assignment Operators:
+// ? Task 6:
+// let num1 = 10;
+// num1 += 1;
+// console.log(num1);
+// // ? Task 7:
+// let num2 = 2;
+// num2 -= 1;
+// console.log(num2);
+
+// ? Activity 3: Comparison Operators:
+// ? Task 8:
+// let a = 4;
+// let b = 10;
+
+// // let c = a > b;
+// let c = a < b;
+// console.log(c);
+// ? Task 9:
+// let a = 4;
+// let b = 10;
+
+// let c = a >= b;
+// // let c = a <= b;
+// console.log(c);
+
+// ? Task 10:
+// let num1 = 3;
+// let num2 = 6;
+
+// let num1 = 3;
+// let num2 = "3";
+// // let num1 = 3;
+// // let num2 = 3;
+// // console.log(num1 == num2);
+// console.log(num1 === num2);
+
+// ? Activity 4: Logical Operators:
+// ? Task 11:
+
+// let a = 5;
+// let b = 9;
+
+// let result = a > b && b > a;
+// console.log(result);
+
+// ? Task 12:
+
+// let a = 5;
+// let b = 10;
+
+// let result = a > b || b > a;
+// console.log(result);
+
+// ? Task 13:
+
+// let isAvailable = true;
+
+// console.log(!isAvailable);
+
+// ? Activity 5: Ternary Operators:
+// ? Task 14:
+let num = -1;
+let result = num >= 0 ? "The number is POSITIVE" : "The number is NEGATIVE";
+console.log(result);
