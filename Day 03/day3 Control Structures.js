dd94fbaab97bdede4040694bcdc63c7e110e7747
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-15 15:59:48 +0500

    final commit

---
+++ Day 03/day3 Control Structures.js
@@ -0,0 +1,49 @@
+// ? Activity 1: if-else Statements
+// ? Task 1:
+// let num = 0;
+// if (num > 0) {
+//   console.log("Number is Positive");
+// } else if (num < 0) {
+//   console.log("Number is Negative");
+// } else {
+//   console.log("Number is zero");
+// }
+// ? Task 2:
+// let age = 18;
+
+// if (age > 0 && age < 18) {
+//   console.log("You are not eligible to vote");
+// } else {
+//   console.log("You are eligible to vote");
+// }
+// ? Activity 2: Nested if-else Statement
+// ? Task 3:
+
+// ? Activity 3: Switch Case:
+// ? Task 4:
+let day = 7;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid Day");
+}
