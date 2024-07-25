a90cfa05bca6ce8f544e57f038f33aebae2e0c33
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-25 09:52:06 +0500

    21 problems added

--- Promises/script.js
+++ Promises/script.js
@@ -121,70 +121,160 @@
 //   .catch((err) => console.log(`${number} is : ` + err));
 
 // ? 13. Create a promise that simulates an API call that sometimes fails and use `.catch()` to handle the error.
-const promise13 = new Promise((res, rej) => {
-  const success = Math.random() > 0.5; // Simulate a 50% chance of failure
-  if (success) {
-    res("https://picsum.photos/v2/list");
-  } else {
-    rej("Error Occurred!");
-  }
-})
-  .then((api) => console.log(api))
-  .catch((err) => console.log(err));
+// const promise13 = new Promise((res, rej) => {
+//   const success = Math.random() > 0.5; // Simulate a 50% chance of failure
+//   if (success) {
+//     res("https://picsum.photos/v2/list");
+//   } else {
+//     rej("Error Occurred!");
+//   }
+// })
+//   .then((api) => console.log(api))
+//   .catch((err) => console.log(err));
 
 // ? 14. Chain two promises where the second promise only executes if the first one resolves successfully;
-let firstName = "M.MUDASSIR ";
-let lastName = "Zaman";
+// let firstName = "M.MUDASSIR ";
+// let lastName = "Zaman";
 
-const promise14 = new Promise((res, rej) => {
-  res(`${firstName}`);
-})
-  .then((frsName) => {
-    return frsName + lastName;
-  })
-  .then((name) => {
-    console.log(name);
-  });
+// const promise14 = new Promise((res, rej) => {
+//   res(`${firstName}`);
+// })
+//   .then((frsName) => {
+//     return frsName + lastName;
+//   })
+//   .then((name) => {
+//     console.log(name);
+//   });
 
 // ? 15. Create a promise that rejects if a given string is empty and resolves otherwise.
-let str = "";
-const promise15 = new Promise((res, rej) => {
-  if (str !== "") res("the String contains " + str);
-  else rej("The given str is empty");
-})
-  .then((string) => console.log(string))
-  .catch((err) => console.log(err));
+// let str = "";
+// const promise15 = new Promise((res, rej) => {
+//   if (str !== "") res("the String contains " + str);
+//   else rej("The given str is empty");
+// })
+//   .then((string) => console.log(string))
+//   .catch((err) => console.log(err));
 // ? 16. Create three promises that resolve with different values after 1, 2, and 3 seconds respectively, and use `Promise.all()` to log the results.
-const promise16a = new Promise((res, rej) => {
-  setTimeout(() => {
-    res("First promise resolved");
-  }, 1000);
-});
-const promise16b = new Promise((res, rej) => {
-  setTimeout(() => {
-    res("Second promise resolved");
-  }, 2000);
-});
-const promise16c = new Promise((res, rej) => {
-  setTimeout(() => {
-    res("Third promise resolved");
-  }, 3000);
-});
+// const promise16a = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("First promise resolved");
+//   }, 1000);
+// });
+// const promise16b = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("Second promise resolved");
+//   }, 2000);
+// });
+// const promise16c = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     res("Third promise resolved");
+//   }, 3000);
+// });
 
-Promise.all([promise16a, promise16b, promise16c])
-  .then((results) => console.log(results))
-  .catch((err) => {
-    console.log(err);
-  });
+// Promise.all([promise16a, promise16b, promise16c])
+//   .then((results) => console.log(results))
+//   .catch((err) => {
+//     console.log(err);
+//   });
 // ? 17. Create two promises that resolve with the values 'First' and 'Second', and use `Promise.all()` to log the combined results.
-const promise17a = new Promise((res, rej) => {
-  res("First");
-});
-const promise17b = new Promise((res, rej) => {
-  res("Second");
-});
-Promise.all([promise17a, promise17b])
-  .then((output) => console.log(output))
-  .catch((err) => console.log(err));
+// const promise17a = new Promise((res, rej) => {
+//   res("First");
+// });
+// const promise17b = new Promise((res, rej) => {
+//   res("Second");
+// });
+// Promise.all([promise17a, promise17b])
+//   .then((output) => console.log(output))
+//   .catch((err) => console.log(err));
 
 // ? 18. Create three promises where one rejects after 2 seconds, and use `Promise.all()` to handle the rejection.
+
+// const promise18a = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     rej("Promise 1 rejected");
+//   }, 2000);
+// });
+// const promise18b = new Promise((res, rej) => {
+//   res("Promise 2 resolved");
+// });
+// const promise18c = new Promise((res, rej) => {
+//   res("Promise 3 resolved");
+// });
+
+// Promise.all([promise18a, promise18b, promise18c])
+//   .then((output) => {
+//     console.log(output);
+//   })
+//   .catch((err) => console.log(err));
+// ? 19. Create five promises that resolve with numbers and use `Promise.all()` to sum the resolved values.
+// const promise19a = new Promise((res, rej) => {
+//   res(2);
+// });
+// const promise19b = new Promise((res, rej) => {
+//   res(4);
+// });
+// const promise19c = new Promise((res, rej) => {
+//   res(6);
+// });
+// const promise19d = new Promise((res, rej) => {
+//   res(8);
+// });
+// const promise19e = new Promise((res, rej) => {
+//   res(10);
+// });
+
+// Promise.all([promise19a, promise19b, promise19c, promise19d, promise19e])
+//   .then((sum) =>
+//     sum.reduce((acc, curr) => {
+//       return acc + curr;
+//     }, 0)
+//   )
+//   .then((result) =>
+//     console.log("Sum of all numbers in the promises: " + result)
+//   )
+//   .catch((err) => console.log(err));
+
+// ? 20. Create four promises that resolve with different strings and use `Promise.all()` to concatenate the strings.
+// const promise20a = new Promise((res, rej) => {
+//   res("Hello! ");
+// });
+// const promise20b = new Promise((res, rej) => {
+//   res("How ");
+// });
+// const promise20c = new Promise((res, rej) => {
+//   res("are ");
+// });
+// const promise20d = new Promise((res, rej) => {
+//   res("you?");
+// });
+
+// Promise.all([promise20a, promise20b, promise20c, promise20d])
+//   .then((strs) =>
+//     strs.reduce((prev, curr) => {
+//       return prev + curr;
+//     })
+//   )
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// ? 21. Create three promises that resolve at different times (1s, 2s, 3s) and use `Promise.race()` to log the result of the fastest one.
+const promise21a = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("First Promise Resolved");
+  }, 1000);
+});
+const promise21b = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("Second Promise Resolved");
+  }, 1000);
+  //   res("Second Promise Resolved");
+});
+const promise21c = new Promise((res, rej) => {
+  setTimeout(() => {
+    res("Third Promise Resolved");
+  }, 1000);
+});
+
+Promise.race([promise21a, promise21b, promise21c])
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
